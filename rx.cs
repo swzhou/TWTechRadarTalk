2797fb4366c2e1c77833d5abf5681ae3acd583ac
qlyao <twer@qlyaos-MacBook-Pro.local>
2013-12-23 00:31:29 +0800

    some change of rx slides

---
+++ rx.cs
@@ -0,0 +1,56 @@
+
+
+public void MeasurementsChanged(int temp, int humidity, int windPower)
+{
+    if (temp > 5)
+    {
+        seedingMachine.Start();
+
+        if (humidity > 65)
+            reapingMachine.Start();
+    }
+
+    if (temp > 10 && humidity < 55 && windPower < 4)
+        wateringMachine.Start();
+} 
+
+
+public class SeedingMachineObserver : IObserver<WeatherData>
+{
+	public void OnNext(WeatherData weatherData)
+	{
+		seedingMachine.Start();
+	}
+}
+
+
+var weatherData = GetWeatherData();
+var seedingMachineObservable = from data in weatherData where data.Temp > 5 select data;
+seedingMachineObservable.Subscribe(new SeedingMachineObserver());
+
+
+
+
+public class Actor<T> : IObserver<T>, IObservable<T>
+{
+	public void Receive(T t)
+	{
+		this.OnNext(t);
+	}
+
+	public void Post(T t)
+	{
+		var receiver = new Actor<T>();
+		receiver.Subscribe(this);
+	}
+}
+
+
+
+
+
+
+
+
+
+
